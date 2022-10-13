4aa03c34dfd6077ad4ddd6b8440722ba367d195e
Deepthi Vijayakumar <deepthi.vijaykumar1@gmail.com>
2022-10-13 08:55:59 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,8 +2,8 @@ module "s3_bucket" {
 source = "terraform-aws-modules/s3-bucket/aws"
 bucket_prefix="Deepthi_Vijayakumar"
 version="2.8.0"
-bucket = "my-s3-bucket"
-  acl    = "private"
+bucket = "s3-bucket"
+acl    = "private"
 
   versioning = {
     enabled = true
